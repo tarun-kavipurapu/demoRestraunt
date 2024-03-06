07fb5058b70b55d1c86ee8bc98608e113268f344
tarun-kavipurapu <tarunsharmakavipurapu@gmail.com>
2024-03-06 20:45:44 +0530

    errors fixed

--- src/main/java/com/example/demo/auth/UserConfig.java
+++ src/main/java/com/example/demo/auth/UserConfig.java
@@ -1,9 +1,10 @@
 package com.example.demo.auth;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+@Configuration
 public class UserConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.UniqueElements;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 /**
  * user_id--> id set by mongodb,
@@ -20,19 +23,19 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @Data
 public class User {
-
-
-
-@NotNull
-
+@NotBlank
     private String fullname;
 
-@NotNull
-
+@Field("userName")
+@NotBlank
 @Indexed(unique = true)
+
     private String username;
-@NotNull
+@NotBlank
+@Field("Password")
+
     private String password;
+@NotBlank
     private Role role;
 
 
